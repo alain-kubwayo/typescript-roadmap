cf0afaa32e25008bd8fb8e39ca44ab61f7f6c6cb
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-18 11:09:34 +0200

    feat: understanding types

--- 02-typescript-basics-and-basic-types/app.ts
+++ 02-typescript-basics-and-basic-types/app.ts
@@ -8,7 +8,11 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string){
 
 const number1 = 5;
 const number2 = 3.8;
+let number3: number;
+number3 = 100;
 const printResult = true;
 const resultPhrase = `Result is: `;
+// let str = 'hello';
+// str = 10;
 
 add(number1, number2, printResult, resultPhrase);
