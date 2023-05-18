d05783e86f1a10cbaef02a283adb5e9648531630
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-18 12:07:19 +0200

    feat: working with enums

--- 02-typescript-basics-and-basic-types/app.js
+++ 02-typescript-basics-and-basic-types/app.js
@@ -2,16 +2,46 @@
 //     name: string;
 //     age: number;
 // } = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Alain',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'Alain',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 };
-// let favoriteActivities: string[];
+// person.role.push('admin'); // push is allowed in tuple unline re-assignment
+// person.role[0] = 'author';
+// console.log(person.role);
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
+}
 var favoriteActivities;
-favoriteActivities = ['hello', 23];
+// let favoriteActivities: any[];
+favoriteActivities = ['hello', 'world'];
+// favoriteActivities = ['hello', 23];
 console.log(person.name, person.age);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.map());
 }
