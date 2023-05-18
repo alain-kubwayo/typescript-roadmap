fea859a40fb24579569c831e09f3779fe7e946b6
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-18 11:33:51 +0200

    feat: add object & nested object types

--- 02-typescript-basics-and-basic-types/app.js
+++ 02-typescript-basics-and-basic-types/app.js
@@ -1,13 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    if (showResult) {
-        console.log(phrase + (n1 + n2));
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 3.8;
-var printResult = true;
-var resultPhrase = "Result is: ";
-add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: 'Alain',
+    age: 30
+};
+console.log(person.name, person.age);
--- 02-typescript-basics-and-basic-types/app.ts
+++ 02-typescript-basics-and-basic-types/app.ts
@@ -1,18 +1,10 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string){
-    if(showResult) {
-        console.log(phrase + (n1 + n2));
-    }else {
-        return n1 + n2;
-    }
-}
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+const person = {
+    name: 'Alain',
+    age: 30
+};
 
-const number1 = 5;
-const number2 = 3.8;
-let number3: number;
-number3 = 100;
-const printResult = true;
-const resultPhrase = `Result is: `;
-// let str = 'hello';
-// str = 10;
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name, person.age);
