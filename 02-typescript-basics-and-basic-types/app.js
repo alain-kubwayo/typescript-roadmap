cdc5493b55729018875f508aea1cd640540b8fa1
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-18 11:41:18 +0200

    feat: add array type

--- 02-typescript-basics-and-basic-types/app.js
+++ 02-typescript-basics-and-basic-types/app.js
@@ -1,5 +1,17 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
 var person = {
     name: 'Alain',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
+// let favoriteActivities: string[];
+var favoriteActivities;
+favoriteActivities = ['hello', 23];
 console.log(person.name, person.age);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- 02-typescript-basics-and-basic-types/app.ts
+++ 02-typescript-basics-and-basic-types/app.ts
@@ -4,7 +4,19 @@
 // } = {
 const person = {
     name: 'Alain',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities: string[];
+// let favoriteActivities: any[];
+
+favoriteActivities = ['hello', 'world'];
+// favoriteActivities = ['hello', 23];
+
 console.log(person.name, person.age);
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map());
+}
