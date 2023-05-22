ce6ecb37b9eb4ffa22dc2d1a823f5901275d104e
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-22 16:40:13 +0200

    feat: objects and arrays

--- 00-netninja-crash-course/app.js
+++ 00-netninja-crash-course/app.js
@@ -9,4 +9,32 @@ age = 40;
 var isBlackBelt = false;
 // console.log(age, isBlackBelt)
 var getCircumference = function (diameter) { return diameter * Math.PI; };
-console.log(getCircumference(17));
+// console.log(getCircumference(17));
+// arrays
+var names = ['alain', 'james'];
+names.push('john');
+// names.push(21); // will throw an error
+// names[0] = 12; // throws an error
+var numbers = [1, 2, 4, 10];
+numbers.push(40);
+// numbers.push('h'); // throws an error
+var mixed = ['ken', 4, 'chun-li', 9];
+mixed.push('ryu');
+mixed.push(38);
+mixed[0] = 123;
+// console.log(mixed);
+// objects
+var ninja = {
+    name: 'james',
+    belt: 'black',
+    age: 32
+};
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30'; // error
+// ninja.skills = ['fighting', 'sneaking']; // can't add additional properties after definition
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
--- 00-netninja-crash-course/app.ts
+++ 00-netninja-crash-course/app.ts
@@ -13,4 +13,40 @@ let isBlackBelt = false;
 // console.log(age, isBlackBelt)
 
 const getCircumference = (diameter: number) => diameter * Math.PI;
-console.log(getCircumference(17));
+// console.log(getCircumference(17));
+
+// arrays
+let names = ['alain', 'james'];
+names.push('john');
+// names.push(21); // will throw an error
+// names[0] = 12; // throws an error
+
+let numbers = [1, 2, 4, 10];
+numbers.push(40);
+// numbers.push('h'); // throws an error
+
+let mixed = ['ken', 4, 'chun-li', 9];
+mixed.push('ryu');
+mixed.push(38);
+mixed[0] = 123
+
+// console.log(mixed);
+
+// objects
+
+let ninja = {
+    name: 'james',
+    belt: 'black',
+    age: 32
+}
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30'; // error
+// ninja.skills = ['fighting', 'sneaking']; // can't add additional properties after definition
+
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+}
