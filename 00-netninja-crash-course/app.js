6ff7a2c6fe098f793943f3225c2bc8954f857cd4
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-22 16:28:16 +0200

    feat: type basics

--- 00-netninja-crash-course/app.js
+++ 00-netninja-crash-course/app.js
@@ -1,5 +1,12 @@
 var character = 'luigi';
-console.log(character);
+// console.log(character)
 var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) { return console.log(input); });
+// console.log(inputs);
+// inputs.forEach(input => console.log(input));
+// Type Basics
+var age = 30;
+age = 40;
+var isBlackBelt = false;
+// console.log(age, isBlackBelt)
+var getCircumference = function (diameter) { return diameter * Math.PI; };
+console.log(getCircumference(17));
--- 00-netninja-crash-course/app.ts
+++ 00-netninja-crash-course/app.ts
@@ -1,6 +1,16 @@
 const character = 'luigi';
-console.log(character)
+// console.log(character)
 const inputs = document.querySelectorAll("input");
-console.log(inputs);
+// console.log(inputs);
 
-inputs.forEach(input => console.log(input));
+// inputs.forEach(input => console.log(input));
+
+// Type Basics
+
+let age = 30;
+age = 40;
+let isBlackBelt = false;
+// console.log(age, isBlackBelt)
+
+const getCircumference = (diameter: number) => diameter * Math.PI;
+console.log(getCircumference(17));
